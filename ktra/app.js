444be4a8f005a619b81db651b4170e6254182071
ANHHOAI2739 <121038226+ANHHOAI2739@users.noreply.github.com>
2023-04-13 00:01:03 +0700

    Add files via upload

---
+++ ktra/app.js
@@ -0,0 +1,44 @@
+const $a = document.getElementById('input-a');
+const $b = document.getElementById('input-b');
+const $result = document.getElementById('result');
+
+
+$result.onclick = () => {
+    let a = $a.value;
+    let b = $b.value;
+    let arr = []
+    // if (a < b ) { alert ('không hợp lệ');    
+    // } else {
+        for ( let i = a; i < b; i++) {
+        } 
+        arr.push(i);
+        
+    }
+// }
+
+
+
+
+
+//bài 2
+// const $inputNumber = document.getElementById("input-number");
+// const $button = document.getElementById("input-button");
+
+
+
+// $button.onclick = () => {
+//     let n = $inputNumber.value;
+//     if (n < 1 || n > 10) { alert ('không hợp lệ');    
+//     } else {
+//         for(let i = 1; i <= n; i++) {
+
+//             for(let j = 1; j <= i; j++) {
+//                 document.write("*");
+//             }
+//             document.write("<br />");
+//         }
+//     }
+        
+//     }
+
+
